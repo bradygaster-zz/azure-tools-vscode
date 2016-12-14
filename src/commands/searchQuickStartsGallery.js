585b6cc7791d7012c3bc505ca007ab23346626df
Brady Gaster <bradyg@microsoft.com>
2016-12-14 00:47:01 -0800

    added full arm search support

--- src/commands/searchQuickStartsGallery.js
+++ src/commands/searchQuickStartsGallery.js
@@ -5,6 +5,7 @@ var github = require('octonode');
 var client = github.client();
 var githubSearch = client.search();
 var constants = config.getConstants();
+var download = require('download-file');
 
 exports.createCommand = function createCommand(state) {
     vscode.commands.registerCommand('searchQuickStartsGallery', function () {
@@ -20,7 +21,7 @@ exports.createCommand = function createCommand(state) {
             state.AzureGallerySearchTerm = searchTerm;
 
             githubSearch.code({
-                q: state.AzureGallerySearchTerm + ':file+repo:azure/azure-quickstart-templates+language:json',
+                q: state.AzureGallerySearchTerm + '+in:file+repo:azure/azure-quickstart-templates+language:json+filename:metadata',
                 sort: 'created',
                 order: 'asc'
             }, (err, results) => {
@@ -28,15 +29,58 @@ exports.createCommand = function createCommand(state) {
                 state.AzureGallerySearchResults = results.items;
                 state.AzureGallerySearchResults.sort(dynamicSort('path'));
                 state.AzureGallerySearchResults.forEach(function (result) {
-                    state.AzureGalleryList.push(result.path);
+                    state.AzureGalleryList.push(result.path.replace('/metadata.json', ''));
                 });
                 vscode.window.showQuickPick(state.AzureGalleryList)
                     .then((selectedItem) => {
                         var selectedTemplate = state.AzureGallerySearchResults.filter((itm) => {
-                            return itm.path == selectedItem;
+                            return itm.path == selectedItem + '/metadata.json';
                         });
-                        if (selectedTemplate && selectedTemplate.length > 0)
-                            console.log(selectedTemplate[0].git_url);
+
+                        if (vscode.workspace.rootPath) {
+                            if (selectedTemplate && selectedTemplate.length > 0)
+                                var options = {
+                                    directory: vscode.workspace.rootPath,
+                                    filename: selectedTemplate[0].name
+                                };
+
+                            var url = selectedTemplate[0].html_url;
+                            url = url.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('blob/', '');
+
+                            // download the template
+                            download(url.replace('metadata.json', 'azuredeploy.json'), {
+                                directory: vscode.workspace.rootPath,
+                                filename: 'azuredeploy.json'
+                            }, function (err) {
+                                if (err) {
+                                    vscode.window.showErrorMessage(constants.promptErrorDownloadingTemplate.replace('{0}', err));
+                                }
+                                else {
+                                    vscode.workspace.openTextDocument(vscode.workspace.rootPath + '\\azuredeploy.json')
+                                        .then(doc => {
+                                            vscode.window.showTextDocument(doc);
+                                        });
+                                }
+                                console.log("file downloaded");
+                            });
+
+                            // download the template parameters
+                            download(url.replace('metadata.json', 'azuredeploy.parameters.json'), {
+                                directory: vscode.workspace.rootPath,
+                                filename: 'azuredeploy.parameters.json'
+                            }, function (err) {
+                                if (err) {
+                                    vscode.window.showErrorMessage(constants.promptErrorDownloadingTemplate.replace('{0}', err));
+                                }
+                                else {
+                                    vscode.workspace.openTextDocument(vscode.workspace.rootPath + '\\azuredeploy.parameters.json')
+                                        .then(doc => {
+                                            vscode.window.showTextDocument(doc);
+                                        });
+                                }
+                                console.log("file downloaded");
+                            });
+                        }
                     });
             });
 
