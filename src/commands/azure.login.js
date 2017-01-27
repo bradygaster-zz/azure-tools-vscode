246148e9c6d84b8f2576de4c8ac7f9eab5dc9c71
Brady Gaster <bradyg@microsoft.com>
2017-01-27 15:11:13 -0800

    removed constants from login

--- src/commands/azure.login.js
+++ src/commands/azure.login.js
@@ -5,8 +5,14 @@ var getUrls = require('get-urls');
 var vscode = require('vscode');
 var ux = require('../ux');
 var config = require('../config');
-var constants = config.getConstants();
 var appEvents = require('../appEvents');
+
+var loginButtonLabel = 'Sign In',
+    enterCodeString = 'enter the code ',
+    authString = ' to authenticate.',
+    signInMessage = 'The code {0} has been copied to your clipboard. Click Sign In and paste in the code to authenticate.',
+    loggedInMessage = 'You have been logged in. Use the \'azure subscription list\' command to select your desired subscription.',
+    promptNoSubscriptionsOrMisconfigured = 'No Azure subscriptions found (are you missing the "azure.tenantId" setting?).';
 
 exports.createCommand = function createCommand(state) {
     vscode.commands.registerCommand('azure.login', function () {
@@ -29,16 +35,16 @@ exports.createCommand = function createCommand(state) {
 
         options.userCodeResponseLogger = function (message) {
             // extract the code to be copied to the clipboard from the message
-            var codeCopied = message.substring(message.indexOf(constants.enterCodeString)
-                + constants.enterCodeString.length).replace(constants.authString, '');
+            var codeCopied = message.substring(message.indexOf(enterCodeString)
+                + enterCodeString.length).replace(authString, '');
             cp.copy(codeCopied);
 
             // show the user the friendly message
             vscode.window.showInformationMessage(
-                constants.signInMessage.replace('{0}', codeCopied), {
-                    title: constants.loginButtonLabel
+                signInMessage.replace('{0}', codeCopied), {
+                    title: loginButtonLabel
                 }).then(function (btn) {
-                    if (btn && btn.title == constants.loginButtonLabel) {
+                    if (btn && btn.title == loginButtonLabel) {
                         open(getUrls(message)[0]);
                     }
                 });
@@ -60,16 +66,16 @@ exports.createCommand = function createCommand(state) {
                 credentials.retrieveTokenFromCache(function (notUsed, tokenType, accessToken) {
                     state.selectedSubscriptionId = state.subscriptions[0].id;
                     state.accessToken = accessToken;
-                    vscode.window.showInformationMessage(constants.loggedInMessage);
+                    vscode.window.showInformationMessage(loggedInMessage);
                     vscode.window.setStatusBarMessage(
-                        constants.statusLoggedInAndSubscriptionSelected.replace('{0}', state.subscriptions[0].name));
+                        statusLoggedInAndSubscriptionSelected.replace('{0}', state.subscriptions[0].name));
                     appEvents.emit('loggedIn', state);
                 });
 
                 ux.getRegions(state);
             }
             else {
-                vscode.window.showErrorMessage(constants.promptNoSubscriptionsOrMisconfigured);
+                vscode.window.showErrorMessage(promptNoSubscriptionsOrMisconfigured);
             }
         });
     });
